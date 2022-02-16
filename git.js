dda5dc5368a36e83790fc0d0abd647363c955e9a
julietablanca <juliietabln@gmail.com>
2022-02-16 10:56:04 -0300

    mensaje

--- git.js
+++ git.js
@@ -1,1 +1,24 @@
 let process = require ('process')
+
+
+/* 
+
+Recuento de comandos
+
+- git --version  = Conocer la version de git
+
+- git init // Inicializar un repositorio de git (una sola vez por proyecto)
+
+- git status // Conocer el estado de git
+- git add . // Agregar archivos al seguimiento de git
+- git commit -m"Mensaje del commit" // Guardar archivos de seguimiento en git
+
+
+ CONFIGURACIONES
+- git config --global user.name "Nombre de usuario de github"
+- git config -- global user.email "Email con el que te logeaste en github" 
+
+
+git log muestra todos los commit
+*/
+
